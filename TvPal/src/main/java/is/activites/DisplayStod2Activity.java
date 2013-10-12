e16de14033ea9fd27d2bd967891a6542630e21a6
Arnar <arnarh91@gmail.com>
2013-10-12 11:13:05 +0000

    Added Custom Adapter to Stöd2 events

--- TvPal/src/main/java/is/activites/DisplayStod2Activity.java
+++ TvPal/src/main/java/is/activites/DisplayStod2Activity.java
@@ -14,9 +14,11 @@ import android.widget.ListView;
 import android.widget.Toast;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import is.datacontracts.EventDataContract;
+import is.handlers.CustomBaseAdapter;
 import is.parsers.Stod2ScheduleParser;
 import is.rules.Helpers;
 import is.tvpal.R;
@@ -30,10 +32,11 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
     public static final String EXTRA_DURATION = "is.activites.DURATION";
 
     private List<EventDataContract> _events;
-    private ArrayAdapter<EventDataContract> _schedulesAdapter;
     private SwipeGestureFilter _detector;
     private ProgressDialog _waitingDialog;
     private String _workingDate;
+    private List<EventDataContract> _todaySchedule;
+    private CustomBaseAdapter _adapterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,7 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
 
     private void Initialize()
     {
+        _todaySchedule = new ArrayList<EventDataContract>();
         _workingDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
 
         Intent intent = getIntent();
@@ -58,7 +62,7 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
 
     public void onItemClick(AdapterView<?> parent, View view, int position, long id)
     {
-        EventDataContract selectedEvent = _schedulesAdapter.getItem(position);
+        EventDataContract selectedEvent = _adapterView.getItem(position);
 
         Intent intent = new Intent(this, DetailedEventActivity.class);
         intent.putExtra(EXTRA_TITLE, selectedEvent.getTitle());
@@ -99,22 +103,23 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
     {
         _workingDate = Helpers.MinusOneDayToDate(_workingDate);
 
-        _schedulesAdapter = new ArrayAdapter<EventDataContract>(this, R.layout.row);
+        _todaySchedule = new ArrayList<EventDataContract>();
 
         for (EventDataContract e : _events)
         {
             if (e.getEventDate().equalsIgnoreCase(_workingDate))
-                _schedulesAdapter.add(e);
+                _todaySchedule.add(e);
         }
 
-        if(_schedulesAdapter.getCount() == 0)
+        if(_todaySchedule.size() == 0)
         {
             Toast.makeText(this, "Schedule not available: " + _workingDate, Toast.LENGTH_SHORT).show();
             _workingDate = Helpers.AddOneDayToDate(_workingDate); //Add one day, so we don't go over the limit
             return;
         }
 
-        setListAdapter(_schedulesAdapter);
+        _adapterView = new CustomBaseAdapter(this, R.layout.listview_item_row, _todaySchedule);
+        setListAdapter(_adapterView);
         Toast.makeText(this, _workingDate, Toast.LENGTH_SHORT).show();
     }
 
@@ -122,22 +127,23 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
     {
         _workingDate = Helpers.AddOneDayToDate(_workingDate);
 
-        _schedulesAdapter = new ArrayAdapter<EventDataContract>(this, R.layout.row);
+        _todaySchedule = new ArrayList<EventDataContract>();
 
         for (EventDataContract e : _events)
         {
             if (e.getEventDate().equalsIgnoreCase(_workingDate))
-                _schedulesAdapter.add(e);
+                _todaySchedule.add(e);
         }
 
-        if(_schedulesAdapter.getCount() == 0)
+        if(_todaySchedule.size() == 0)
         {
             Toast.makeText(this, "Schedule not available: " + _workingDate, Toast.LENGTH_SHORT).show();
             _workingDate = Helpers.MinusOneDayToDate(_workingDate); //Minus one day, so we don't go over the limit
             return;
         }
 
-        setListAdapter(_schedulesAdapter);
+        _adapterView = new CustomBaseAdapter(this, R.layout.listview_item_row, _todaySchedule);
+        setListAdapter(_adapterView);
         Toast.makeText(this, _workingDate, Toast.LENGTH_SHORT).show();
     }
 
@@ -174,8 +180,8 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
         @Override
         protected void onPostExecute(String result)
         {
-            setListAdapter(_schedulesAdapter);
-
+            _adapterView = new CustomBaseAdapter(ctx, R.layout.listview_item_row, _todaySchedule);
+            setListAdapter(_adapterView);
             _waitingDialog.dismiss();
         }
 
@@ -186,12 +192,10 @@ public class DisplayStod2Activity extends ListActivity implements AdapterView.On
                 Stod2ScheduleParser parser = new Stod2ScheduleParser(myurl);
                 _events = parser.GetSchedules();
 
-                _schedulesAdapter = new ArrayAdapter<EventDataContract>(ctx, R.layout.row);
-
                 for (EventDataContract e : _events)
                 {
                     if (e.getEventDate().equalsIgnoreCase(_workingDate))
-                        _schedulesAdapter.add(e);
+                        _todaySchedule.add(e);
                 }
             }
             catch (Exception ex)
