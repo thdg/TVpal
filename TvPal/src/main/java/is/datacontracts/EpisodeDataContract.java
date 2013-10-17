8c1716c1a31e4e06ed984e9c90c31f5ef445409a
Arnar <arnarh91@gmail.com>
2013-10-17 17:09:20 +0000

    Created Episode parser & datacontract

---
+++ TvPal/src/main/java/is/datacontracts/EpisodeDataContract.java
@@ -0,0 +1,28 @@
+package is.datacontracts;
+
+/**
+ * Created by Arnar on 17.10.2013.
+ */
+public class EpisodeDataContract
+{
+    private String episodeId;
+    private String seasonNumber;
+    private String showNumber;
+    private String aired;
+    private String overview;
+
+    public String getEpisodeId() { return episodeId; }
+    public void setEpisodeId(String episodeId) { this.episodeId = episodeId;}
+
+    public String getSeasonNumber() { return seasonNumber; }
+    public void setSeasonNumber(String seasonNumber) { this.seasonNumber = seasonNumber; }
+
+    public String getShowNumber() { return showNumber; }
+    public void setShowNumber(String showNumber) { this.showNumber = showNumber; }
+
+    public String getAired() { return aired; }
+    public void setAired(String aired) { this.aired = aired; }
+
+    public String getOverview() { return overview; }
+    public void setOverview(String overview) { this.overview = overview; }
+}
---
+++ TvPal/src/main/java/is/parsers/TvDbEpisodeParser.java
@@ -0,0 +1,95 @@
+package is.parsers;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import is.datacontracts.EpisodeDataContract;
+
+/**
+ * Created by Arnar on 12.10.2013.
+ */
+public class TvDbEpisodeParser extends DefaultHandler {
+
+    private String baseURL;
+    private List<EpisodeDataContract> episodes;
+    private String tmpValue;
+    private EpisodeDataContract episodeTmp;
+
+    public TvDbEpisodeParser(String baseUrl)
+    {
+        this.baseURL = baseUrl;
+        this.episodes = new ArrayList<EpisodeDataContract>();
+    }
+
+    public List<EpisodeDataContract> GetEpisodes()
+    {
+        parseDocument();
+
+        return this.episodes;
+    }
+
+    private void parseDocument()
+    {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+
+        try
+        {
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(baseURL, this);
+        }
+        catch (ParserConfigurationException e)
+        {
+            e.printStackTrace();
+        }
+        catch (SAXException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void startElement(String s, String s1, String elementName, Attributes attributes) throws SAXException
+    {
+        if (elementName.equalsIgnoreCase("Episode"))
+            episodeTmp = new EpisodeDataContract();
+    }
+
+    @Override
+    public void endElement(String s, String s1, String element) throws SAXException
+    {
+        if(element.equalsIgnoreCase("id"))
+            episodeTmp.setEpisodeId(tmpValue);
+
+        if(element.equalsIgnoreCase("DVD_season"))
+            episodeTmp.setSeasonNumber(tmpValue);
+
+        if(element.equalsIgnoreCase("absolute_number"))
+            episodeTmp.setShowNumber(tmpValue);
+
+        if(element.equalsIgnoreCase("FirstAired"))
+            episodeTmp.setAired(tmpValue);
+
+        if(element.equalsIgnoreCase("Overview"))
+            episodeTmp.setOverview(tmpValue);
+
+        if(element.equalsIgnoreCase("episode"))
+            episodes.add(episodeTmp);
+    }
+
+    @Override
+    public void characters(char[] ac, int i, int j) throws SAXException
+    {
+        tmpValue = new String(ac, i, j);
+    }
+}
