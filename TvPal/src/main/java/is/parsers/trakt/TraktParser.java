314bf0a169bf4a90ff85f8424eb6c01efbbb4a64
Arnar <arnarh91@gmail.com>
2013-11-15 16:31:57 +0000

    Upgraded Gradle to 1.8, added Gson (Json library) to dependencies - Þið þurfið að uppfæra Android Studio í nýjastu útgáfu til að fá þetta til að virka, hann á að dl líka nýja Gradle

--- TvPal/src/main/java/is/parsers/trakt/TraktParser.java
+++ TvPal/src/main/java/is/parsers/trakt/TraktParser.java
@@ -1,8 +1,23 @@
 package is.parsers.trakt;
 
+import android.os.AsyncTask;
+import com.google.gson.Gson;
+
 /**
  * Created by Arnar on 15.11.2013.
  */
 public class TraktParser
 {
+    public static String TraktUrl = "http://api.trakt.tv/shows/trending.json/f0e3af66061e47b3243e25ed7b6443ca";
+
+
+    private class GetTrendingShows extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            Gson data = new Gson();
+            return null;
+        }
+    }
 }
