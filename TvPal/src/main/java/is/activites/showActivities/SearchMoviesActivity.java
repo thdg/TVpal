60f3533eedb4f13f3ee943b492b56ecb20a8fdd9
Arnar <arnarh91@gmail.com>
2013-11-22 19:17:02 +0000

    Some killer look improvements

--- TvPal/src/main/java/is/activites/showActivities/SearchMoviesActivity.java
+++ TvPal/src/main/java/is/activites/showActivities/SearchMoviesActivity.java
@@ -1,7 +1,5 @@
 package is.activites.showActivities;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,7 +9,7 @@ import android.support.v4.view.ViewPager;
 import is.activites.baseActivities.BaseFragmentActivity;
 import is.tvpal.R;
 
-public class SearchMoviesActivity extends BaseFragmentActivity implements ActionBar.TabListener
+public class SearchMoviesActivity extends BaseFragmentActivity
 {
     private ViewPager mViewPager;
 
@@ -28,8 +26,6 @@ public class SearchMoviesActivity extends BaseFragmentActivity implements Action
     {
         SearchPagerAdapter mScheduleAdapter = new SearchPagerAdapter(getSupportFragmentManager(), this);
 
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
         mViewPager = (ViewPager) findViewById(R.id.pagerSearch);
@@ -39,29 +35,10 @@ public class SearchMoviesActivity extends BaseFragmentActivity implements Action
             @Override
             public void onPageSelected(int position)
             {
-                actionBar.setSelectedNavigationItem(position);
+                mViewPager.setCurrentItem(position);
             }
         });
-
-        for (int i = 0; i < mScheduleAdapter.getCount(); i++)
-        {
-            actionBar.addTab(actionBar.newTab()
-                    .setText(mScheduleAdapter.getPageTitle(i))
-                    .setTabListener(this));
-        }
     }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
 
     public class SearchPagerAdapter extends FragmentStatePagerAdapter
     {
--- TvPal/src/main/java/is/activites/showActivities/SearchShowsActivity.java
+++ TvPal/src/main/java/is/activites/showActivities/SearchShowsActivity.java
@@ -1,20 +1,15 @@
 package is.activites.showActivities;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.MenuItem;
-
 import is.activites.baseActivities.BaseFragmentActivity;
 import is.tvpal.R;
 
-public class SearchShowsActivity extends BaseFragmentActivity implements ActionBar.TabListener
+public class SearchShowsActivity extends BaseFragmentActivity
 {
     private ViewPager mViewPager;
 
@@ -22,7 +17,7 @@ public class SearchShowsActivity extends BaseFragmentActivity implements ActionB
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.swipe_series);
+        setContentView(R.layout.swipe_search);
 
         Initialize();
     }
@@ -31,41 +26,19 @@ public class SearchShowsActivity extends BaseFragmentActivity implements ActionB
     {
         SearchPagerAdapter mScheduleAdapter = new SearchPagerAdapter(getSupportFragmentManager(), this);
 
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mViewPager = (ViewPager) findViewById(R.id.pagerSeries);
+        mViewPager = (ViewPager) findViewById(R.id.pagerSearch);
         mViewPager.setAdapter(mScheduleAdapter);
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
         {
             @Override
             public void onPageSelected(int position)
             {
-                actionBar.setSelectedNavigationItem(position);
+                mViewPager.setCurrentItem(position);
             }
         });
-
-        for (int i = 0; i < mScheduleAdapter.getCount(); i++)
-        {
-            actionBar.addTab(actionBar.newTab()
-                    .setText(mScheduleAdapter.getPageTitle(i))
-                    .setTabListener(this));
-        }
     }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {}
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {}
 
     public class SearchPagerAdapter extends FragmentStatePagerAdapter
     {
