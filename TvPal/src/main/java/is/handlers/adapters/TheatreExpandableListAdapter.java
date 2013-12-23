58363e5926ab25b6b75cc49cdd9f662ca378cd57
Arnar <arnarh91@gmail.com>
2013-12-23 20:31:47 +0000

    Fixed Expandeble LV adapter

--- TvPal/src/main/java/is/handlers/adapters/TheatreExpandableListAdapter.java
+++ TvPal/src/main/java/is/handlers/adapters/TheatreExpandableListAdapter.java
@@ -6,104 +6,106 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
-
 import java.util.List;
-
 import is.contracts.datacontracts.cinema.CinemaShowtimes;
 import is.tvpal.R;
 
-/**
- * Created by thorsteinn on 11/21/13.
- */
 public class TheatreExpandableListAdapter extends BaseExpandableListAdapter {
 
-    private List<CinemaShowtimes> _showtimes;
-    private Context _context;
+    private List<CinemaShowtimes> mShowTimes;
+    private Context mContext;
 
-    public TheatreExpandableListAdapter(Context context, List<CinemaShowtimes> showtimes) {
-        this._context = context;
-        this._showtimes = showtimes;
+    public TheatreExpandableListAdapter(Context context, List<CinemaShowtimes> showtimes)
+    {
+        this.mContext = context;
+        this.mShowTimes = showtimes;
     }
 
     @Override
-    public int getGroupCount() {
-        return this._showtimes.size();
+    public int getGroupCount()
+    {
+        return this.mShowTimes.size();
     }
 
     @Override
-    public int getChildrenCount(int groupPos) {
+    public int getChildrenCount(int groupPos)
+    {
         CinemaShowtimes group = (CinemaShowtimes) this.getGroup(groupPos);
         return group.getSchedule().size();
     }
 
     @Override
-    public Object getGroup(int groupPos) {
-        return this._showtimes.get(groupPos);
+    public Object getGroup(int groupPos)
+    {
+        return this.mShowTimes.get(groupPos);
     }
 
     @Override
-    public Object getChild(int groupPos, int childPos) {
+    public Object getChild(int groupPos, int childPos)
+    {
         CinemaShowtimes group = (CinemaShowtimes) this.getGroup(groupPos);
         return group.getSchedule().get(childPos);
     }
 
     @Override
-    public long getGroupId(int groupPos) {
+    public long getGroupId(int groupPos)
+    {
         return groupPos;
     }
 
     @Override
-    public long getChildId(int groupPos, int childPos) {
+    public long getChildId(int groupPos, int childPos)
+    {
         return childPos;
     }
 
     @Override
-    public boolean hasStableIds() {
+    public boolean hasStableIds()
+    {
         return false;
     }
 
     @Override
-    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup)
+    {
         CinemaShowtimes cinema = (CinemaShowtimes) this.getGroup(i);
         String theater = cinema.getTheater();
 
-        if (view == null) {
-            LayoutInflater infalInflater = (LayoutInflater) this._context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (view == null) 
+        {
+            LayoutInflater infalInflater = (LayoutInflater) this.mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = infalInflater.inflate(R.layout.listview_cinema_theatre, null);
         }
 
-        TextView name = (TextView) view.findViewById(R.id.theatreName);
-
-        name.setText(theater);
-
+        ((TextView) view.findViewById(R.id.theatreName)).setText(theater);
         return view;
     }
 
     @Override
-    public View getChildView(int i, int i2, boolean b, View view, ViewGroup viewGroup) {
-        String child = (String) getChild(i, i2);
+    public View getChildView(int groupPosition, int childPosition, boolean b, View view, ViewGroup viewGroup)
+    {
+        String child = (String) getChild(groupPosition, childPosition);
         String[] childList = child.split(" ");
         String hours = childList[0];
-        String hall = childList.length>1 ? childList[1] : "";
 
-        if (view == null) {
-            LayoutInflater infalInflater = (LayoutInflater) this._context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        int index = child.lastIndexOf("(");
+        String hall = index != -1 ? child.substring(index) : "";
+
+        if (view == null)
+        {
+            LayoutInflater infalInflater = (LayoutInflater) this.mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = infalInflater.inflate(R.layout.listview_cinema_showtime, null);
         }
 
-        TextView hoursView = (TextView) view.findViewById(R.id.showtimeShowtime);
-        TextView hallView = (TextView) view.findViewById(R.id.showtimeHall);
-
-        hoursView.setText(hours);
-        hallView.setText(hall);
+        ((TextView) view.findViewById(R.id.showtimeShowtime)).setText(hours);
+        ((TextView) view.findViewById(R.id.showtimeHall)).setText(hall);
 
         return view;
     }
 
     @Override
-    public boolean isChildSelectable(int i, int i2) {
+    public boolean isChildSelectable(int i, int i2)
+    {
         return false;
     }
 }
